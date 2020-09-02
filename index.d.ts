7c5a1ca19cd239c0e418e073fb997b8e0aef425c
Oliver Radini <oliver.radini@wheelright.co.uk>
2020-09-02 13:52:32 +0100

    updated type for options

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,20 @@
-declare function deepmerge<T>(x: Partial<T>, y: Partial<T>, options?: deepmerge.Options): T;
-declare function deepmerge<T1, T2>(x: Partial<T1>, y: Partial<T2>, options?: deepmerge.Options): T1 & T2;
+declare function deepmerge<T>(x: Partial<T>, y: Partial<T>, options?: deepmerge.Options<T>): T;
+declare function deepmerge<T1, T2>(x: Partial<T1>, y: Partial<T2>, options?: deepmerge.Options<T1>): T1 & T2;
+
+type Merger<T, K extends keyof T> = (a: T[K], b: T[K]) => T[K];
+type Mergers<T> = {
+  [P in keyof T]: Merger<T, P>
+}
 
 declare namespace deepmerge {
-	export interface Options {
-		arrayMerge?(target: any[], source: any[], options?: Options): any[];
+	export interface Options<T> {
+		arrayMerge?(target: any[], source: any[], options?: Options<T>): any[];
 		clone?: boolean;
-		customMergeFunctions?: {
-			[key:string]: (x: any, y: any) => any,
-		};
+		customMergeFunctions?: Mergers<T>
 		isMergeableObject?(value: object): boolean;
 	}
 
-	export function all (objects: object[], options?: Options): object;
+	export function all<T> (objects: object[], options?: Options<T>): object;
 }
 
 export = deepmerge;
